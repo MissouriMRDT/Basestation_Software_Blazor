fceb688998a486f0396abad2302102dab03b16c2
mjjm4d <threehalves1@gmail.com>
2024-09-02 23:03:59 -0500

    component functionality and sync

---
+++ Basestation_Software.Web/Core/Services/CounterService.cs
@@ -0,0 +1,38 @@
+﻿namespace Basestation_Software.Web.Core.Services
+{
+	public class CounterService
+	{
+		// Injected services
+		private readonly HttpClient _HttpClient;
+
+		private int count = 0;
+
+		public delegate Task CountListener();
+		private event CountListener? _countNotifier;
+
+		// Constructor
+		public CounterService(HttpClient httpClient)
+		{
+			_HttpClient = httpClient;
+		}
+
+		public void CountUp()
+		{
+			count++;
+			if (_countNotifier is not null)
+			{
+				_countNotifier.Invoke();
+			}
+		}
+
+		public void Subscribe(CountListener listener)
+		{
+			_countNotifier += listener;
+		}
+
+		public int GetCount()
+		{
+			return count;
+		}
+	}
+}
