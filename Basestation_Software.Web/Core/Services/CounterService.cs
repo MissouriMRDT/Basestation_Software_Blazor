6e62ede6ce8ea993e93458e0bafd33dbf35af13a
sam_hajdukiewicz <samanthahajdukiewicz@gmail.com>
2024-09-27 18:48:28 -0500

    I created the Counter component for the demo

---
+++ Basestation_Software.Web/Core/Services/CounterService.cs
@@ -0,0 +1,32 @@
+﻿namespace Basestation_Software.Web.Core.Services
+{
+	public class CounterService
+	{
+		private int count;
+		private event Func<Task>? CountNotifier;
+		public CounterService() 
+		{
+			count = 0;
+		}
+		public void CountUp() 
+		{
+			count++;
+		}
+		public int GetCount()
+		{
+			return count;
+		}
+
+		public void Subscribe(Func<Task> listener)
+		{
+			CountNotifier += listener;
+		}
+
+		public void Unsubscribe(Func<Task> listener)
+		{
+			CountNotifier -= listener;
+		}
+
+		
+	}
+}
