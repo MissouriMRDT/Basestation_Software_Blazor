cdc37224763db86f4dd5ce04824f26a80bb9e3cb
Marshall Reinkemeyer <marshallreinkemeyer4@gmail.com>
2024-10-21 18:07:21 -0500

    Added PingTool component. Added PingService.

---
+++ Basestation_Software.Web/Core/Services/PingService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace Basestation_Software.Web.Core.Services;
+
+public class PingService
+{
+    public async Task<string> PingAsync(string ipAddress)
+    {
+        using (var ping = new Ping())
+        {
+            try
+            {
+                var reply = await ping.SendPingAsync(ipAddress, 1000); // 1 second timeout
+                if (reply.Status == IPStatus.Success)
+                {
+                    ping.Dispose();
+                    return $"Ping to {ipAddress} successful: Time = {reply.RoundtripTime} ms";
+                }
+                else
+                {
+                    ping.Dispose();
+                    return $"Ping to {ipAddress} failed: {reply.Status}";
+                }
+            }
+            catch (Exception ex)
+            {
+                ping.Dispose();
+                return $"Error pinging {ipAddress} : {ex.Message}";
+            }
+        }
+    }
+}
