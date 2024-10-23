6d7ba338c755fd2fc5c00fbc546ad6f2c7b0099e
sam_hajdukiewicz <samanthahajdukiewicz@gmail.com>
2024-10-23 10:55:28 -0500

    Moved the DataItem class to .Models

--- Basestation_Software.Web/Core/Services/RamanGraphService.cs
+++ Basestation_Software.Web/Core/Services/RamanGraphService.cs
@@ -1,15 +1,6 @@
 ﻿namespace Basestation_Software.Web.Core.Services
 {
-	public class RamanGraphService
-	{
-        class DataItem
-        {
-            public int Raman_Shift { get; set; }
-            public double Intensity { get; set; }
-        }
-
-
-    }
+	public class RamanGraphService{}
 	
 
 }
