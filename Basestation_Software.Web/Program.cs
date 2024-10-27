9499175f29b6c027767505bc3404859bfc9b5831
Brendan Westley <brendan@westley.us>
2024-10-26 21:25:10 -0500

    Cleanup pages

--- Basestation_Software.Web/Program.cs
+++ Basestation_Software.Web/Program.cs
@@ -35,14 +35,6 @@ builder.Services.AddBlazoredToast();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
