fa3382877903bfce2de1c913430853b367ff2935
Marshall Reinkemeyer <ILiveForSTEM@gmail.com>
2024-11-19 16:07:02 -0600

    Added modal to warn of a poor connection.

--- Basestation_Software.Web/Core/Services/PingService.cs
+++ Basestation_Software.Web/Core/Services/PingService.cs
@@ -12,18 +12,8 @@ public class PingService
         using (var ping = new Ping())
         {
             var reply = await ping.SendPingAsync(ipAddress, 1000); // 1 second timeout
-            if (reply.Status == IPStatus.Success)
-            {
-                ping.Dispose();
-                return reply;
-                //return $"Ping to {ipAddress} successful: Time = {reply.RoundtripTime} ms";
-            }
-            else
-            {
-                ping.Dispose();
-                return reply;
-                //return $"Ping to {ipAddress} failed: {reply.Status}";
-            }
+            ping.Dispose();
+            return reply;
         }
     }
 }
