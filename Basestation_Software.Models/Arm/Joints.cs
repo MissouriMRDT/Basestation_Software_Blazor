6293be91806a92f068113f37aaecf06e7ec9e468
mjjm4d <threehalves1@gmail.com>
2025-01-27 21:49:14 -0600

    Add Arm Angular layout

---
+++ Basestation_Software.Models/Arm/Joints.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basestation_Software.Models.Arm;
+public class Joints
+{
+    // This could be implemented to rovecomm manifest at some point maybe
+    public static readonly string[] JointNames = ["X", "Y1", "Y2", "Z", "Pitch", "R1", "R2"];
+}
