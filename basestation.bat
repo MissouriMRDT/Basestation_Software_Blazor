e6caf5c780cd0ef3e191563eccc26cc253876cf1
mjjm4d <threehalves1@gmail.com>
2025-02-17 15:04:54 -0600

    Fix batch file

--- basestation.bat
+++ basestation.bat
@@ -1,5 +1,4 @@
-winget install Microsoft.DotNet.DesktopRuntime.9
-winget install Microsoft.DotNet.AspNetCore.9
+REM winget install dotnet-sdk-8
 start "Server" dotnet run --project Basestation_Software.Api
 start "Client" dotnet run --project Basestation_Software.Web
 start "" http://localhost:8080
