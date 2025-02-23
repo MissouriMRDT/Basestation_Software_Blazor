43fa29839c5be397644a99dbad8606639f4ea9f1
mjjm4d <threehalves1@gmail.com>
2025-02-23 08:50:31 -0600

    Map rover icon

--- Basestation_Software.Web/wwwroot/components.js
+++ Basestation_Software.Web/wwwroot/components.js
@@ -59,7 +59,7 @@ class roverMap {
         this.lMap.on('zoomend', this.onZoomLevelChange.bind(this));
         this.lMap.on('moveend', this.onZoomLevelChange.bind(this));
         this.waypointLayerGroup = L.layerGroup([]).addTo(this.lMap);
-        this.roverIcon = L.marker([37.951897, -91.777587]).addTo(this.waypointLayerGroup);
+        this.roverIcon = L.marker([37.951764, -91.778441], { icon: new L.icon({ iconUrl: "images/marker.png", iconSize: [50, 50] }) }).addTo(this.lMap);
 
     }
     // Call component.OnZoomLevel.
