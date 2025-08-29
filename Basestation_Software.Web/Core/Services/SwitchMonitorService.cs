4fa62cb43f620c477cd9b06de3240fa45476d8a9
OcelotEmpire <hobbz.pi@gmail.com>
2025-08-29 06:25:30 +0000

    Display port statuses and parse eigrp topology

--- Basestation_Software.Web/Core/Services/SwitchMonitorService.cs
+++ Basestation_Software.Web/Core/Services/SwitchMonitorService.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using RoveComm;
 using Renci.SshNet;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using System.Text;
 
 namespace Basestation_Software.Web.Core.Services;
 
@@ -61,7 +62,7 @@ public class SwitchMonitorService
         public override string ToString()
         {
             return $"""
-            Interface: {Name}, Type: {Type.ToString()}, Description: {Description ?? "None"}, IP: {Ip ?? "Unspecified"}, Status: {Status.ToString()}
+            Interface: {Name}, Type: {Type}, Description: {Description ?? "None"}, IP: {Ip ?? "Unspecified"}, Status: {Status}
               Input:
                 Last 5 Minutes: {Traffic.FiveMinuteInputRatePackets} packets ({Traffic.FiveMinuteInputRateBits} bits) per second
                 Total: {Traffic.TotalInputPackets} packets ({Traffic.TotalInputBytes} bytes)
@@ -72,12 +73,74 @@ public class SwitchMonitorService
         }
     }
 
+    public class PortStatus
+    {
+        public string Port { get; set; } = "";
+        public bool Connected { get; set; }
+        public int? VLan { get; set; }
+        public bool Routed
+        {
+            get => VLan is null;
+            set => VLan = value ? null : 1;
+        }
+        public string Speed { get; set; } = "Unknown";
+        public string Type { get; set; } = "Unknown";
+
+        public override string ToString()
+        {
+            return $"""
+            Port: {Port}, Connected: {Connected}, Vlan: {(Routed ? "Routed" : VLan)}, Speed: {Speed}, Type: {Type}
+            """;
+        }
+    }
+
+    public class VlanInfo
+    {
+        public int VLan { get; set; }
+        public string Name { get; set; } = "";
+        public string Status { get; set; } = "Unknown";
+        public List<string> Ports { get; set; } = [];
+    }
+
+    public enum EigrpStatus
+    {
+        Passive, Active, Update, Query, Reply, ReplyStatus, SiaStatus
+    }
+    public class EigrpSuccessorInfo
+    {
+        public string NextHopIp { get; set; } = "";
+        public int FeasibleDistance { get; set; }
+        public int AdvertisedDistance { get; set; }
+        public string OutgoingInterface { get; set; } = "";
+    }
+    public class EigrpTopologyInfo
+    {
+        public string DestinationIp { get; set; } = "";
+        public EigrpStatus Status;
+        public List<EigrpSuccessorInfo> Successors { get; set; } = [];
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"Remote Network: {DestinationIp}, Status: {Status}\n");
+            foreach (var successor in Successors)
+            {
+                sb.Append($"  via Remote Port: {successor.NextHopIp}, Outgoing Interface: {successor.OutgoingInterface}, FD: ({successor.AdvertisedDistance}/{successor.FeasibleDistance})\n");
+            }
+            return sb.ToString();
+        }
+    }
+
     public SwitchMonitorService()
     {
         Console.WriteLine("!!!!!!!!!!!!!!!!!!!!NUTS!!!!!!!!!!!!!!");
         // GetInterfaces();
     }
 
+    /// <summary>
+    /// Get a list of interfaces and their network traffic statistics.
+    /// Parses the Cisco command SHOW INTERFACES
+    /// </summary>
+    /// <returns>A list of interfaces</returns>
     public static List<InterfaceInfo> GetInterfaces()
     {
         var interfaces = new List<InterfaceInfo>();
@@ -94,7 +157,7 @@ public class SwitchMonitorService
         using SshCommand command = client.RunCommand("show interfaces");
         string result = command.Result;
         var interfaceEntryPattern = new Regex(@"([a-zA-Z0-9_\-/]+) is (up|down|administratively down), line protocol is (up|down)");
-        var typePattern = new Regex(@"Hardware is (EtherSVI|Fast Ethernet|Gigabit Ethernet|Loopback)");
+        var typePattern = new Regex(@"Hardware is (.+?),");
         var descriptionPattern = new Regex(@"Description: (.*?)[\r\n]");
         var ipPattern = new Regex(@"Internet address is (\d+\.\d+\.\d+\.\d+/\d+)");
         var fiveMinutePattern = new Regex("""
@@ -120,12 +183,14 @@ public class SwitchMonitorService
             {
                 "up" => InterfaceStatus.Up,
                 "down" => InterfaceStatus.Down,
-                "administratively down" => InterfaceStatus.Disabled
+                "administratively down" => InterfaceStatus.Disabled,
+                _ => InterfaceStatus.Disabled
             };
             info.ProtocolStatus = interfaceMatch.Groups[3].Value switch
             {
                 "up" => ProtocolStatus.Up,
-                "down" => ProtocolStatus.Down
+                "down" => ProtocolStatus.Down,
+                _ => ProtocolStatus.Down
             };
             var typeMatch = typePattern.Match(interfaceText);
             info.Type = typeMatch.Groups[1].Value switch
@@ -153,14 +218,14 @@ public class SwitchMonitorService
             {
                 info.Traffic = new TrafficInfo
                 {
-                    FiveMinuteInputRateBits = Int32.Parse(fiveMinuteMatch.Groups[1].Value),
-                    FiveMinuteInputRatePackets = Int32.Parse(fiveMinuteMatch.Groups[2].Value),
-                    FiveMinuteOutputRateBits = Int32.Parse(fiveMinuteMatch.Groups[3].Value),
-                    FiveMinuteOutputRatePackets = Int32.Parse(fiveMinuteMatch.Groups[4].Value),
-                    TotalInputPackets = Int32.Parse(totalInputMatch.Groups[1].Value),
-                    TotalInputBytes = Int32.Parse(totalInputMatch.Groups[2].Value),
-                    TotalOutputPackets = Int32.Parse(totalOutputMatch.Groups[1].Value),
-                    TotalOutputBytes = Int32.Parse(totalOutputMatch.Groups[2].Value),
+                    FiveMinuteInputRateBits = int.Parse(fiveMinuteMatch.Groups[1].Value),
+                    FiveMinuteInputRatePackets = int.Parse(fiveMinuteMatch.Groups[2].Value),
+                    FiveMinuteOutputRateBits = int.Parse(fiveMinuteMatch.Groups[3].Value),
+                    FiveMinuteOutputRatePackets = int.Parse(fiveMinuteMatch.Groups[4].Value),
+                    TotalInputPackets = int.Parse(totalInputMatch.Groups[1].Value),
+                    TotalInputBytes = int.Parse(totalInputMatch.Groups[2].Value),
+                    TotalOutputPackets = int.Parse(totalOutputMatch.Groups[1].Value),
+                    TotalOutputBytes = int.Parse(totalOutputMatch.Groups[2].Value),
                 };
             }
             else
@@ -172,12 +237,167 @@ public class SwitchMonitorService
 
             interfaceMatch = nextMatch;
         }
+        Console.WriteLine("==========SHOW INTERFACES==========");
         foreach (var info in interfaces)
         {
             Console.WriteLine(info);
         }
 
         return interfaces;
+    }
+
+    /// <summary>
+    /// Get the EIGRP topology table. Runs the Cisco command SHOW IP EIGRP TOPOLOGY
+    /// </summary>
+    /// <returns>A list of EIGRP entries</returns>
+    public static List<EigrpTopologyInfo> GetEigrpTopology()
+    {
+        var topology = new List<EigrpTopologyInfo>();
+        using var client = new SshClient(SwitchIP, SwitchUser, SwitchPassword);
+        try
+        {
+            client.Connect();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Could not connect: {ex.Message}");
+            return topology;
+        }
+        using SshCommand command = client.RunCommand("show ip eigrp topology");
+        string result = command.Result;
+
+        var eigrpEntryPattern = new Regex(@"([PAUQRrs]) (\d+\.\d+\.\d+\.\d+/\d+), (\d+) successors, FD is (\d+)");
+        var eigrpSuccessorPattern = new Regex(@"\s*via (Connected|\d+\.\d+\.\d+\.\d+) \((\d+)/(\d+)\), ([a-zA-Z0-9_\-/]+)");
+
+        var eigrpMatch = eigrpEntryPattern.Match(result);
+        while (eigrpMatch.Success)
+        {
+            // Get substring of result corresponding to this interface entry
+            var nextMatch = eigrpMatch.NextMatch();
+            int eigrpTextLength = nextMatch.Success ?
+                nextMatch.Index - eigrpMatch.Index
+                : result.Length - eigrpMatch.Index;
+            string eigrpText = result.Substring(eigrpMatch.Index, eigrpTextLength);
+
+            var eigrpEntry = new EigrpTopologyInfo
+            {
+                Status = eigrpMatch.Groups[1].Value switch
+                {
+                    "P" => EigrpStatus.Passive,
+                    "A" => EigrpStatus.Active,
+                    "U" => EigrpStatus.Update,
+                    "Q" => EigrpStatus.Query,
+                    "R" => EigrpStatus.Reply,
+                    "r" => EigrpStatus.ReplyStatus,
+                    "s" => EigrpStatus.SiaStatus,
+                    _ => EigrpStatus.Passive
+                },
+                DestinationIp = eigrpMatch.Groups[2].Value
+            };
+
+            var eigrpSuccessorMatches = eigrpSuccessorPattern.Matches(eigrpText);
+            foreach (Match successor in eigrpSuccessorMatches)
+            {
+                eigrpEntry.Successors.Add(new EigrpSuccessorInfo
+                {
+                    NextHopIp = successor.Groups[1].Value,
+                    FeasibleDistance = int.Parse(successor.Groups[2].Value),
+                    AdvertisedDistance = int.Parse(successor.Groups[3].Value),
+                    OutgoingInterface = successor.Groups[4].Value
+                });
+            }
+
+            topology.Add(eigrpEntry);
+            eigrpMatch = nextMatch;
+        }
+
+        Console.WriteLine("==========SHOW IP EIGRP TOPOLOGY==========");
+        foreach (var entry in topology)
+        {
+            Console.WriteLine(entry);
+        }
+        return topology;
+    }
+
+    /// <summary>
+    /// Get VLan port assignments
+    /// </summary>
+    /// <returns>A list of VLans with the ports assigned to them</returns>
+    // public static List<VlanInfo> GetVLanAssignments()
+    // {
+
+    // }
+
+    /// <summary>
+    /// Get port statuses including their connectedness and VLan assignments.
+    /// Runs the Cisco command SHOW INTERFACE STATUS
+    /// </summary>
+    /// <returns>A list of port statuses</returns>
+    public static List<PortStatus> GetPorts()
+    {
+        var ports = new List<PortStatus>();
+        using var client = new SshClient(SwitchIP, SwitchUser, SwitchPassword);
+        try
+        {
+            client.Connect();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Could not connect: {ex.Message}");
+            return ports;
+        }
+        using SshCommand command = client.RunCommand("show interface status");
+        string result = command.Result;
+        // Port      Name               Status       Vlan       Duplex  Speed Type 
+        // Fa1/1     AutonomyAndSensorA notconnect   3            auto   auto 10/100BaseTX 
+        var headerMatch = Regex.Match(result, @"(Port)\s+(Name)\s+(Status)\s+(Vlan)\s+(Duplex)\s+(Speed)\s+(Type)");
+        if (!headerMatch.Success)
+        {
+            return ports;
+        }
+        using (var reader = new StringReader(result))
+        {
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                // Skip table header
+                if (line.StartsWith("Port"))
+                {
+                    continue;
+                }
+                var headings = headerMatch.Groups;
+                var port = new PortStatus
+                {
+                    Port = line[headings[1].Index..headings[2].Index].Trim(),
+                    // Name = line[headings[2].Index..headings[3].Index].Trim(),
+                    Connected = line[headings[3].Index..headings[4].Index].Trim() switch
+                    {
+                        "connected" => true,
+                        "notconnect" => false,
+                        _ => false
+                    },
+                    Speed = line[(headings[5].Index + headings[5].Length)..(headings[6].Index + headings[6].Length)].Trim(),
+                    Type = line[headings[7].Index..].Trim()
+                };
+                var vlanPattern = new Regex(@"(routed|\d+)");
+                var vlanMatch = vlanPattern.Match(line, headings[4].Index);
+                if (vlanMatch.Success)
+                {
+                    if (int.TryParse(vlanMatch.Groups[1].Value, out int vlan))
+                    {
+                        port.VLan = vlan;
+                    }
+                }
+                ports.Add(port);
+            }
+        }
+        Console.WriteLine("==========SHOW INTERFACE STATUS==========");
+        foreach (var port in ports)
+        {
+            Console.WriteLine(port);
+        }
+
+        return ports;
     }
 
 }
