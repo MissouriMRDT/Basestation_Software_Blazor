c51f1c4b42b484cdfbe0a8afe07b8c1defb58e54
Mars Rover Local Account <marsroverbs@rs786pz84.managed.mst.edu>
2025-09-07 21:49:37 -0500

    BasestationComputer20250907

--- basestation.bat
+++
@@ -1,4 +0,0 @@
-REM winget install dotnet-sdk-8
-start "Server" dotnet run --project Basestation_Software.Api
-start "Client" dotnet run --project Basestation_Software.Web
-start "" http://localhost:8080
