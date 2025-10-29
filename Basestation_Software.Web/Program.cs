dbf9487fee3296237e7d6a587991db3dab665682
mjjm4d <threehalves1@gmail.com>
2025-10-29 12:07:09 -0500

    Add JSONGrapher razor and service file

--- Basestation_Software.Web/Program.cs
+++ Basestation_Software.Web/Program.cs
@@ -47,6 +47,7 @@ builder.Services.AddSingleton<ArmControlService>();
 builder.Services.AddRoveComm();
 builder.Services.AddBlazoredToast();
 builder.Services.AddSingleton<LoggerService>();
+builder.Services.AddSingleton<FileReaderService>();
 
 var app = builder.Build();
 
