57a848a0c713f5bb51ae2619a045ab7ccc4a28c4
Brendan Westley <brendan@westley.us>
2026-05-19 12:13:01 -0500

    Add more camera types, rename cameras

--- Basestation_Software.Web/Program.cs
+++ Basestation_Software.Web/Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddServerSideBlazor()
         .AddHubOptions(option => option.MaximumReceiveMessageSize = 10_000_000); // Configures the message size for SignalR connections.
 builder.Services.AddScoped<CookieService>();
 builder.Services.AddSingleton<VRService>();
+builder.Services.AddSingleton<CameraService>();
 builder.Services.AddSingleton<GPSWaypointState>();
 builder.Services.AddSingleton<PingService>();
 builder.Services.AddSingleton<OpService>();
